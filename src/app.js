65c2dda2484739de147be52d1eb3082d16d6461e
Patricio Morel <patricio.morel@outlook.com>
2021-06-08 07:46:44 -0400

    Refactored tracking services into a more generic object

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
 import StorageService from './storage.js'
 
 const storage = new StorageService()
-
-
+const trackers = {
+    'easy'      : item => `https://www.easy.cl/tienda/seguimiento?orderId=${item.info}`,
+    'cupoclick' : item => `https://api.enviame.io/s2/companies/3773/deliveries/${item.info}/tracking`,
+    'spdigital' : item => {
+        const parsedInfo = item.info.split('-')
+        return `http://seguimiento.spdigital.cl/?web_order=${parsedInfo[0]}&check=${parsedInfo[1]}&button=`
+    }
+}
 function addTracking(){
     const store     = document.querySelector('#retail').value
     const tracknum  = document.querySelector('#idseguimiento').value
@@ -14,21 +20,7 @@ function fillList(){
     const list = document.querySelector('#compras')
 
     for(let item of storage.readAll()){
-        let url = ''
-        switch(item.store){
-            case 'easy':
-                url = `https://www.easy.cl/tienda/seguimiento?orderId=${item.tracknum}`
-                break
-            case 'cupoclick':
-                url = `https://api.enviame.io/s2/companies/3773/deliveries/${item.tracknum}/tracking`
-                break
-            case 'spdigital':
-                const parsedInfo = item.tracknum.split('-')
-                url = `http://seguimiento.spdigital.cl/?web_order=${parsedInfo[0]}&check=${parsedInfo[1]}&button=`
-                break
-
-        }
-        list.appendChild( createListNode(item.store, item.tracknum, url) )
+        list.appendChild( createListNode(item.store, item.info, trackers[item.store](item)) )
     }
 
 }
